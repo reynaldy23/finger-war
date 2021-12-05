e567f5c632d56d037ad0217afcec240d773b67c3
Reynaldy <reynaldykwok@gmail.com>
2021-12-05 23:21:39 +0700

    inherited checkpoint

---
+++ lib/inherited.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class Updating extends InheritedWidget {
+  final int counter;
+
+  const Updating({
+    Key? key,
+    required Widget child,
+    required this.counter,
+  }) : super(key: key, child: child);
+
+  static Updating of(BuildContext context) {
+    final Updating? result = context.dependOnInheritedWidgetOfExactType<Updating>();
+    assert(result != null, 'No Updating found in context');
+    return result!;
+  }
+
+  @override
+  bool updateShouldNotify(Updating oldWidget) {
+    return oldWidget.counter != counter;
+  }
+}

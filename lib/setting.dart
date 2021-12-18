705c030ea8dc3c8a2d0ce025b543147d09b3b7e5
Reynaldy <reynaldykwok@gmail.com>
2021-12-18 21:58:07 +0700

    update test

--- lib/setting.dart
+++ lib/setting.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:toggle_switch/toggle_switch.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 import 'inherited.dart';
 
